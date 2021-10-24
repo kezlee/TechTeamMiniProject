926646354cd743f76914692168233c1cfcc9ab21
Quentin Bitschene <qbitschene@hugeinc.com>
2021-10-24 18:25:18 +0800

    update on codebase

--- server/server.js
+++ server/server.js
@@ -1,31 +1,29 @@
 var WebSocketServer = require('websocket').server;
 var http = require('http');
 
-var server = http.createServer(function(request, response) {
-  // process HTTP request. Since we're writing just WebSockets
-  // server we don't have to implement anything.
-});
-server.listen(8080, function() { });
+var server = http.createServer();
+server.listen(8080);
 
 // create the server
 wsServer = new WebSocketServer({
+  // You must specify an httpServer on which to mount the WebSocket server.
   httpServer: server
 });
 
 // WebSocket server
 wsServer.on('request', function(request) {
   var connection = request.accept(null, request.origin);
+  
+  console.log('WebSocket Connected !');
 
   // This is the most important callback for us, we'll handle
   // all messages from users here.
   connection.on('message', function(message) {
-    console.log('message received')
-    if (message.type === 'utf8') {
-      // process WebSocket message
-    }
+    console.log('message received:', message.utf8Data)
   });
 
   connection.on('close', function(connection) {
     // close user connection
+    console.log('WebSocket closed !');
   });
 });
